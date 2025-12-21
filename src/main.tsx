62d609e3df1d24aa4f2ec06a511d7492d710bf60
Shon Portugez <shonportugez@gmail.com>
2025-12-21 10:40:12 +0200

    feature: added external custom theme for mui

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {CssBaseline, InitColorSchemeScript} from "@mui/material";
+import AppTheme from "./theme/AppTheme.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <InitColorSchemeScript />
+      <AppTheme>
+          <CssBaseline />
+          <App />
+      </AppTheme>
   </StrictMode>,
 )
